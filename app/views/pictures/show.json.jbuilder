f3d7c40e7e43624c77913409e581413ef56821cc
omarsancas <omarsanchezcas@gmail.com>
2016-02-27 23:15:17 -0600

    Picture status

---
+++ app/views/pictures/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @picture, :id, :hashtag, :image_url, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :pictures
+
   resources :product_types
 
   resources :products
